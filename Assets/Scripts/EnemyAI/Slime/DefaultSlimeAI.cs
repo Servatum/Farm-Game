7b846787da208afa9d1f699f8e056844025b6192
Servatum <bcarl3459@gmail.com>
2024-02-12 10:39:26 -0600

    Added Machete and other s
    Added the machete and edited the slimes script

--- Assets/Scripts/EnemyAI/Slime/DefaultSlimeAI.cs
+++ Assets/Scripts/EnemyAI/Slime/DefaultSlimeAI.cs
@@ -3,10 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class DefaultSlimeAI : MonoBehaviour
+public class DefaultSlimeAI : MonoBehaviour, IDamagable
 {
     public Transform player;
     private NavMeshAgent agent;
+
+    [Header("Stats")]
+    public int health;
+    public ItemData[] dropOnDeath;
+
+    //components
+    private SkinnedMeshRenderer[] meshRenderers;
+
+    void Awake()
+    {
+        meshRenderers = GetComponentsInChildren<SkinnedMeshRenderer>();
+    }
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,4 +32,41 @@ public class DefaultSlimeAI : MonoBehaviour
     {
         agent.destination = player.position;
     }
+    public void TakePhysicalDamage(int damageAmount)
+    {
+        health -= damageAmount;
+
+        if(health <=0)
+        {
+            Die();
+        }
+
+        StartCoroutine(DamageFlash());
+    }
+
+    void Die()
+    { 
+        for(int x = 0; x < dropOnDeath.Length; x++)
+        {
+            Instantiate(dropOnDeath[x].dropPrefab, transform.position, Quaternion.identity);
+        }
+
+        Destroy(gameObject);
+    }
+
+    IEnumerator DamageFlash()
+    {
+        for(int x = 0; x < meshRenderers.Length; x++)
+        {
+            meshRenderers[x].material.color = new Color(1.0f, 0.6f, 0.6f);
+        }
+
+        yield return new WaitForSeconds(0.1f);
+
+        for (int x = 0; x < meshRenderers.Length; x++)
+        {
+            meshRenderers[x].material.color = Color.white;
+        }
+    }
+
 }
